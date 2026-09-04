c267194e7e6e5468496aa2bc946d751678564fbc
FabricioYV <fabricioyauri0903@gmail.com>
2026-09-04 10:55:35 -0500

    Eliminar todo lo relacionado a Yape

--- src/main/java/org/fabricioyv/purchase/RankPurchaseTask.java
+++ src/main/java/org/fabricioyv/purchase/RankPurchaseTask.java
@@ -1,5 +1,6 @@
 package org.fabricioyv.purchase;
 
-public class RankPurchaseTask {
+public class
+RankPurchaseTask {
     // Este archivo ha sido eliminado - La funcionalidad de compras ha sido removida del plugin para ahorrar memoria
 }
